0067549cc0c1bad3eee88e141a7788dd30226386
djyk1005 <djyk1005@gmail.com>
2017-09-12 19:25:21 -0400

    Update singleLineFollow.ino

--- code/milestone 1/singleLineFollow.ino
+++ code/milestone 1/singleLineFollow.ino
@@ -10,8 +10,6 @@ void setup() {
   
   myservo1.attach(9);
   myservo2.attach(10);
-  myservo1.write(90);
-  myservo2.write(90);
 }
 
 // the loop routine runs over and over again forever:
@@ -23,17 +21,24 @@ void loop() {
  Serial.print(F("  "));
  Serial.println(sen2);
 
- if (abs(sen1-sen2)<20){
- myservo1.write(100);
- myservo2.write(80);
+ /*for (int i = 0 ; i< 100 ; i++){
+   myservo1.write(100);
+  myservo2.write(80);
+ }*/
+ //myservo1.write(90);
+ //myservo2.write(90);
+
+ if (abs(sen1-sen2)<70){
+ myservo1.write(0);
+ myservo2.write(180);
  }
  else if (sen1>sen2){
- myservo1.write(100);
+ myservo1.write(0);
  myservo2.write(0);
  }
  else if (sen2>sen1){
   myservo1.write(180);
- myservo2.write(80);
+  myservo2.write(180);
  }
  
   
