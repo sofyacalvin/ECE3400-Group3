7f0b55cf5c005f89f958890de012d11700ed58ac
djyk1005 <djyk1005@gmail.com>
2017-09-15 11:34:43 -0400

    updated single line follow after connecting common ground

--- code/milestone 1/singleLineFollow.ino
+++ code/milestone 1/singleLineFollow.ino
@@ -1,15 +1,18 @@
 #include <Servo.h>
 
 
-Servo myservo1;
-Servo myservo2;
+Servo leftservo;
+Servo rightservo;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  //pinMode(3,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(11,OUTPUT);
   
-  myservo1.attach(9);
-  myservo2.attach(10);
+  leftservo.attach(9);
+  rightservo.attach(11);
+
+  
 }
 
 // the loop routine runs over and over again forever:
@@ -25,20 +28,20 @@ void loop() {
    myservo1.write(100);
   myservo2.write(80);
  }*/
- //myservo1.write(90);
- //myservo2.write(90);
+ //myservo1.write(100);
+ //myservo2.write(80);
 
- if (abs(sen1-sen2)<70){
- myservo1.write(0);
- myservo2.write(180);
+ if (abs(sen1-sen2)<75){
+ leftservo.write(180);
+ rightservo.write(0);
  }
- else if (sen1>sen2){
- myservo1.write(0);
- myservo2.write(0);
+ else if (sen1>sen2){ //tilted to the right; right sensor senses white
+ leftservo.write(90);
+ rightservo.write(0);
  }
- else if (sen2>sen1){
-  myservo1.write(180);
-  myservo2.write(180);
+ else if (sen2>sen1){ //tilted to the left; left sensor senses white
+  leftservo.write(180);
+  rightservo.write(90);
  }
  
   
