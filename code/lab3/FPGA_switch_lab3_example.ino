1f64f880c5697987425c41366a00302ef0527039
DylanMachado <31416435+DylanMachado@users.noreply.github.com>
2017-10-12 23:35:48 -0400

    Add files via upload

---
+++ code/lab3/FPGA_switch_lab3_example.ino
@@ -0,0 +1,25 @@
+
+int pin1 = 12;
+int pin2 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pin1, OUTPUT);
+pinMode(pin2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, LOW);
+  delay(1500);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, HIGH);
+  delay(1500);
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, LOW);
+  delay(1500);
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, HIGH);
+  delay(1500);
+}
