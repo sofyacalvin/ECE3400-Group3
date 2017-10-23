87245cf4a07122670bb957fb578d11c0847980e7
DylanMachado <31416435+DylanMachado@users.noreply.github.com>
2017-10-23 16:01:43 -0400

    Update DFS.ino

--- code/milestone 3/DFS.ino
+++ code/milestone 3/DFS.ino
@@ -97,3 +97,68 @@ void addFrontier(){
   }
 }
 
+/*
+// starting from bottom left (refer to map)
+void addFrontier(){
+  unsigned char next;
+  if (orientation == 0) { //facing east
+    if (1){//south is path
+      next = x_pos << 2 | y_pos + 1;
+      frontier.push(next);
+    }
+    if (1) { //north is path
+      next = x_pos << 2 | y_pos - 1;
+      frontier.push(next);
+    }
+    if (1){//east is path--most prioritized
+      //assuming receieved data ignores "state data"
+      next = x_pos + 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+  }
+  
+  else if (orientation == 1) { // facing south
+    if (1) { //south path
+      next = x_pos << 2 | y_pos + 1;
+      frontier.push(next);
+    }
+    if(1) { // west path
+      next = x_pos - 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+    if (1) { //east
+      next = x_pos + 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+  }
+  
+  else if (orientation == 2) { // facing west
+    if (1) { //south path
+      next = x_pos << 2 | y_pos + 1;
+      frontier.push(next);
+    }
+    if (1) { //west path
+      next = x_pos - 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+    if(1) { // north path
+      next = x_pos << 2 | y_pos - 1;
+      frontier.push(next);
+    }
+  }
+  else if (orientation == 3) { // facing north
+    if(1) { // west path
+      next = x_pos - 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+    if (1) { //north path
+      next = x_pos << 2 | y_pos - 1;
+      frontier.push(next);
+    }
+    if (1) { //east path
+      next = x_pos + 1 << 2 | y_pos;
+      frontier.push(next);
+    }
+  }
+}
+*/
