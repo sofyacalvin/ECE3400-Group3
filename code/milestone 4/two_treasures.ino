62f35dbae7cc43f1b0b28a68be7dbf34150323bf
Amanda Trang <31416491+at669@users.noreply.github.com>
2017-11-11 18:23:04 -0500

    Add files via upload
    set to 64 bins, sets "treasure" variable to 1 for respective bits (17kHz | 12kHz | 7kHz), toggles between two treasure sensors

---
+++ code/milestone 4/two_treasures.ino
@@ -0,0 +1,75 @@
+/*
+fft_adc_serial.pde
+guest openmusiclabs.com 7.7.14
+example sketch for testing the fft library.
+it takes in data on ADC0 (Analog0) and processes them
+with the fft. the data is sent out over the serial
+port at 115.2kb.
+*/
+
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 64 // set to 256 point fft
+
+#include <FFT.h> // include the library
+int treasures = B000;
+
+void setup() {
+  Serial.begin(115200); // use the serial port
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x43; // use adc3
+  DIDR0 = 0x04; // turn off the digital input for adc3
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  //while(1) { // reduces jitter
+    if (ADMUX == 0x43){
+      ADMUX = 0x44;
+      DIDR0 = 0x05;
+    }
+    else if (ADMUX == 0x44){
+      ADMUX = 0x43;
+      DIDR0 = 0x04;
+    }
+  
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 128 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    //Serial.println("start");
+    //Serial.println(ADMUX);
+    for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+      //Serial.println(fft_log_out[i]); // send out the data      
+    }
+    
+    if (fft_log_out[12] > 100) { //7kHz
+      treasures |= 1;
+    }
+    if (fft_log_out[20] > 100) { //12kHz      
+      treasures |= 1 << 1;
+    }
+    if (fft_log_out[28] > 100) { //17kHz
+      treasures |= 1 << 2;
+    }        
+    else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    Serial.println(treasures); // send out the data
+    
+  }
+//}
